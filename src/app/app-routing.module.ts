6d66e8292dfea128c951ca86cadf117c297c4977
alamin <iaam@liveimplevista.onmicrosoft.com>
2025-11-30 23:09:43 +0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './component/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"",redirectTo:"home",pathMatch:"full"},
+  {path:"home",component: HomeComponent},
+  {path:"about",component: AboutComponent},
+  // {path:"home",loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent)},
+  // {path:"about",loadComponent:()=>import('./component/about/about.component').then(m=>m.AboutComponent)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
