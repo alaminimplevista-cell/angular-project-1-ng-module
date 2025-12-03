a1b763f1fd1b0fc18c03c325765fcbc60727ec3e
Md Al Amin <iaam@liveimplevista.onmicrosoft.com>
2025-12-03 17:36:51 +0600

    feat: add About and Admin components with routing, integrate Bootstrap styles and scripts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule, Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './component/about/about.component';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+
 
 const routes: Routes = [
   // {path:"",redirectTo:"home",pathMatch:"full"},
-  {path:"home",component: HomeComponent},
-  {path:"about",component: AboutComponent},
-  // {path:"home",loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent)},
-  // {path:"about",loadComponent:()=>import('./component/about/about.component').then(m=>m.AboutComponent)},
+  // {path:"home",component: HomeComponent},
+  // {path:"about",component: AboutComponent},
+  {path:"",loadComponent:()=>import('./home/home.component').then(m=>m.HomeComponent)},
+  {path:"about",loadComponent:()=>import('./about/about.component').then(m=>m.AboutComponent)},
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      preloadingStrategy:PreloadAllModules
+    }
+
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
